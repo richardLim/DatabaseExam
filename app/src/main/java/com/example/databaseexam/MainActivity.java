f1c85cfbf3a82da55850bb0de34d3c487a152799
putnagy <putnagy@gmail.com>
2021-02-03 12:31:42 +0900

    내용 수정

--- app/src/main/java/com/example/databaseexam/MainActivity.java
+++ app/src/main/java/com/example/databaseexam/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.databaseexam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cursoradapter.widget.CursorAdapter;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MemoActivity.class));
+            }
+        }) ;
+
+        ListView listView = findViewById(R.id.memo_list);
+        MemoDbHelper dbHelper = MemoDbHelper.getInstance(this);
+        Cursor cursor = dbHelper.getReadableDatabase().query(MemoContract.MemoEntry.TABLE_NAME, null, null, null, null, null, null);
+
+        MemoAdapter adapter = new MemoAdapter(this, cursor);
+        listView.setAdapter(adapter);
+    }
+
+    private static class MemoAdapter extends CursorAdapter {
+
+        public MemoAdapter(Context context, Cursor c) {
+            super(context, c, false);
+        }
+
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+            return LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, parent, false);
+        }
+
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+            TextView titleText = view.findViewById(android.R.id.text1);
+            titleText.setText(cursor.getString(cursor.getColumnIndexOrThrow(MemoContract.MemoEntry.COLUMN_NAME_TITLE)));
+        }
     }
 }
--- app/src/main/java/com/example/databaseexam/MemoActivity.java
+++ app/src/main/java/com/example/databaseexam/MemoActivity.java
@@ -2,13 +2,41 @@ package com.example.databaseexam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MemoActivity extends AppCompatActivity {
+    private EditText mTitleEditText;
+    private EditText mContentsEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo);
+        mTitleEditText = findViewById(R.id.title_edit);
+        mContentsEditText = findViewById(R.id.contents_edit);
+    }
+
+    @Override
+    public void onBackPressed() {
+        String title = mTitleEditText.getText().toString();
+        String contents = mContentsEditText.getText().toString();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MemoContract.MemoEntry.COLUMN_NAME_TITLE, title);
+        contentValues.put(MemoContract.MemoEntry.COLUMN_NAME_CONTENTS, contents);
+
+        SQLiteDatabase db = MemoDbHelper.getInstance(this).getWritableDatabase();
+        long newRowId = db.insert(MemoContract.MemoEntry.TABLE_NAME, null, contentValues);
+
+        if (newRowId == -1) {
+            Toast.makeText(this, "저장에 문제가 발생하였습니다.", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "메모가 저장되었습니다.", Toast.LENGTH_SHORT).show();
+        }
+        super.onBackPressed();
     }
 }
